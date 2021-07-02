926eefab5228341d502f53293a91912d929d8ab9
vanejiang <vanejiang@tencent.com>
2021-07-02 20:57:20 +0800

    update

---
+++ cpp/src/solutions/P-00023.cc
@@ -0,0 +1,15 @@
+#include "src/include/list_node.h"
+
+#include <gtest/gtest.h>
+
+namespace leetcode {
+class P00023 : public ::testing::Test {
+ protected:
+  ListNode* mergeKLists(vector<ListNode*>& lists) {
+    return nullptr;
+  }
+};
+
+TEST_F(P00023, Solution) {
+}
+}  // namespace leetcode
