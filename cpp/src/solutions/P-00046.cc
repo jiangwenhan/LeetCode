e652e3f3d3d564876e680aa1bb1a16cdb83c2a3a
vanejiang <vanejiang@tencent.com>
2021-07-12 11:56:56 +0800

    P-00046

---
+++ cpp/src/solutions/P-00046.cc
@@ -0,0 +1,48 @@
+#include <gtest/gtest.h>
+
+namespace leetcode {
+class P00046 : public ::testing::Test {
+ protected:
+  std::vector<std::vector<int>> permute(std::vector<int>& nums) {
+    std::vector<std::vector<int>> result;
+    dfs(result, nums, 0);
+    return result;
+  }
+
+ private:
+  void dfs(std::vector<std::vector<int>>& result, std::vector<int>& nums, int index) {
+    if (index >= nums.size()) {
+      result.emplace_back(nums);
+    }
+    for (int i = index; i < nums.size(); ++i) {
+      swap(nums, i, index);
+      dfs(result, nums, index + 1);
+      swap(nums, i, index);
+    }
+  }
+
+  void swap(std::vector<int>& nums, int i, int j) {
+    int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+  }
+};
+
+TEST_F(P00046, Solution) {
+  std::vector<std::vector<int>> cases = {
+      std::vector<int>{1, 2, 3},
+      std::vector<int>{0, 1},
+      std::vector<int>{1},
+  };
+
+  for (auto& each : cases) {
+    auto result = permute(each);
+    for (auto& res : result) {
+      for (auto item : res) {
+        std::cout << item << ",";
+      }
+      std::cout << std::endl;
+    }
+  }
+}
+}  // namespace leetcode
