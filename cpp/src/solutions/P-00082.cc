66cc17f60e7f6a6b7279731c9ddee2a1f7a000d3
Wenhan Jiang <jiangvane@gmail.com>
2021-07-24 18:08:47 +0800

    P-00082

--- cpp/src/solutions/P-00082.cc
+++ cpp/src/solutions/P-00082.cc
@@ -1,20 +1,56 @@
 #include <gtest/gtest.h>
+#include <deque>
 #include "src/include/input.h"
-#include "src/include/output.h"
 #include "src/include/list_node.h"
+#include "src/include/output.h"
 
 namespace leetcode {
 class P00082 : public ::testing::Test {
  protected:
   ListNode* deleteDuplicates(ListNode* head) {
-    return head;
+    if (head == nullptr) return head;
+    std::deque<ListNode*> stack;
+    stack.push_front(head);
+    ListNode* curr = head->next;
+    bool pop_top = false;
+    while (curr != nullptr) {
+      ListNode* top = stack.front();
+      if (top->val == curr->val) {
+        pop_top = true;
+      } else {
+        if (pop_top) {
+          stack.pop_front();
+        }
+        if (stack.size() != 0) {
+          stack.front()->next = curr;
+        }
+        stack.push_front(curr);
+        pop_top = false;
+      }
+      curr = curr->next;
+    }
+
+    if (pop_top) {
+      stack.pop_front();
+    }
+
+    if (stack.size() != 0) {
+        stack.front()->next = nullptr;
+    } else {
+      return nullptr;
+    }
+
+    return stack.back();
   }
 };
 
 TEST_F(P00082, Solution) {
   std::vector<std::string> cases{
-    "[1,1,2]",
-    "[1,1,2,3,3]",
+      // "[1,1,2]",
+      // "[1,1,2,3,3]",
+      // "[1,2,3,3,4,4,5]",
+      // "[1,1,1,2,3]",
+      "[1,1]",
   };
 
   for (auto each : cases) {
@@ -22,7 +58,10 @@ TEST_F(P00082, Solution) {
     auto res = AToLinkedList(&head, each.data());
     std::cout << "res:" << res << std::endl;
     if (res == 0) {
-      PrintLinkedList(deleteDuplicates(&head));
+      auto result = deleteDuplicates(&head);
+      if (result != nullptr) {
+        PrintLinkedList(result);
+      }
     }
   }
 }
